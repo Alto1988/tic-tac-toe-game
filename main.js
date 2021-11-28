ba54c755200d20e2baf0c0c53ba993afbfa1637f
Jacob Pagan <jake2415@gmail.com>
2021-11-27 19:44:39 -0600

    End of the day

--- main.js
+++ main.js
@@ -1,1 +1,60 @@
 console.log("You're in....");
+/**
+ * TODO: 
+    - for tomorrow make the board interactive
+    - if there is time see if you can wire in some game logic to find the winner of the game.
+ */
+
+let gameState = {
+  gameIsStarted: false,
+  playerOne: {
+    wins: 0,
+  },
+  playerTwo: {
+    wins: 0,
+  },
+};
+
+//INITIAL SETUP
+const startGameButton = document.querySelector(".start-button");
+
+const resetGameButton = document.querySelector(".reset-button");
+
+const playerOneScore = document.querySelector("#player-one-score");
+const playerTwoScore = document.querySelector("#player-two-score");
+
+playerOneScore.innerText = gameState.playerOne.wins;
+playerTwoScore.innerText = gameState.playerTwo.wins;
+
+//END OF SETUP
+
+//Testing players scores
+const increaseButton = document.querySelector("#increase-test");
+
+// increaseButton.addEventListener("click", () => {
+//   gameState.playerOne.wins++;
+//   playerOneScore.innerText = gameState.playerOne.wins;
+//   game.gameState.gameIsStarted = false;
+// });
+
+//Starting the game
+startGameButton.addEventListener("click", () => {
+  if (gameState.gameIsStarted === true) {
+    return;
+  }
+  gameState.gameIsStarted = true;
+  console.log(gameState);
+  console.log("Game is started");
+});
+
+//Resetting the game
+resetGameButton.addEventListener("click", () => {
+  if (gameState.gameIsStarted === false) {
+    return;
+  }
+  gameState.gameIsStarted = false;
+  console.log(gameState);
+  console.log("Game is reset");
+});
+
+console.log(startGameButton);
